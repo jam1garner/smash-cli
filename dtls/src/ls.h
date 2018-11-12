de0e960bd4fbce50c80d41639629b4439455570a
jam1garner <jam1.mcleod@hotmail.com>
2018-11-12 14:03:01 -0500

    Add LS entry reading

--- dtls/src/ls.h
+++ dtls/src/ls.h
@@ -3,43 +3,58 @@
 
 // Definitions of LS file structs
 typedef struct LS_HEADER {
-	char magic[2];
-	uint16_t version;
-   	uint32_t entryCount;	
+    char magic[2];
+    uint16_t version;
+    uint32_t entryCount;    
 } LS_HEADER;
 
 typedef struct LS_ENTRY {
-	uint32_t crc;
-	uint32_t dataOffset;
-	uint32_t length;
+    uint32_t crc;
+    uint32_t dataOffset;
+    uint32_t length;
 } LS_ENTRY;
 
 typedef struct LS_ENTRY_V2 {
-	uint32_t crc;
-	uint32_t dataOffset;
-	uint32_t length;
-	uint16_t dtIndex;
-	uint16_t unk;
+    uint32_t crc;
+    uint32_t dataOffset;
+    uint32_t length;
+    uint16_t dtIndex;
+    uint16_t unk;
 } LS_ENTRY_V2;
 
 // Definitions for LS interfacing
 typedef struct LS_ENTRY_NODE {
-	struct LS_ENTRY_NODE *next;
-	union {
-		LS_ENTRY entry;
-		LS_ENTRY_V2 entry_v2;
-	};
+    union {
+        LS_ENTRY entry;
+        LS_ENTRY_V2 entry_v2;
+    };
+    struct LS_ENTRY_NODE *next;
 } LS_ENTRY_NODE;
 
 typedef struct LS_FILE {
-	LS_HEADER header;
-	LS_ENTRY_NODE *first;
+    LS_HEADER header;
+    LS_ENTRY_NODE *first;
 } LS_FILE;
 
 
 // Provided functions
 LS_FILE *ls_read(FILE *file){
     LS_FILE *lsFile = malloc(sizeof(LS_FILE));
-	fread(lsFile, sizeof(LS_HEADER), 1, file);
-	return lsFile;	
+    fread(lsFile, sizeof(LS_HEADER), 1, file);
+    LS_ENTRY_NODE *current;
+    if(lsFile->header.entryCount){
+        lsFile->first = malloc(sizeof(LS_ENTRY_NODE));
+        current = lsFile->first;
+    }
+    size_t entrySize = (lsFile->header.version == 2) ?
+                           sizeof(LS_ENTRY_V2) :
+                           sizeof(LS_ENTRY);
+    for(int i = 0; i < lsFile->header.entryCount; i++){
+        fread(current, entrySize, 1, file);
+        if(i != lsFile->header.entryCount - 1){
+            current->next = malloc(sizeof(LS_ENTRY_NODE));
+            current = current->next;
+        }
+    }
+    return lsFile;  
 }
--- dtls/src/main.c
+++ dtls/src/main.c
@@ -5,9 +5,18 @@
 int main(int argc, char **argv){
     FILE* file = fopen("/home/jam/Downloads/ls", "rb");
     LS_FILE *lsFile = ls_read(file);
-	printf("Magic - %.*s\n", 2, (char*)lsFile);
-	printf("Version - %i\n", lsFile->header.version);
-	printf("Entry Count - %i\n", lsFile->header.entryCount);
+    printf("Magic - %.*s\n", 2, (char*)lsFile);
+    printf("Version - %i\n", lsFile->header.version);
+    printf("Entry Count - %X\n", lsFile->header.entryCount);
+    printf("------------------\n");
+    LS_ENTRY_NODE *current = lsFile->first;
+    while(current){
+        printf("CRC - %08X\n", current->entry.crc);
+        printf("Data Offset - %X\n", current->entry.dataOffset);
+        printf("Length - %X\n", current->entry.length);
+        printf("------------------\n");
+        current = current->next;
+    }
     free(lsFile);
     return 0;
 }
